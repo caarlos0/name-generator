d04312019ed8b173304bd87346e9a30b2e0fdeb4
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-05-20 10:46:21 -0300

    feat: add ?separator
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- index.go
+++ index.go
@@ -4,12 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/docker/docker/pkg/namesgenerator"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	name := namesgenerator.GetRandomName(0)
+	separator, ok := r.URL.Query()["separator"]
+	if ok {
+		name = strings.ReplaceAll(name, "_", separator[0])
+	}
+
 	switch r.Header.Get("accept") {
 	case "application/json":
 		out, err := json.Marshal(map[string]string{"name": name})
